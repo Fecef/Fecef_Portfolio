506e2d67835974792cc95bc1e18541dfc7777134
Fecef <fecef.figueiredo@gmail.com>
2023-03-28 17:10:55 -0300

    refac: update portofolio general info.

---
+++ src/utils/stackData.ts
@@ -0,0 +1,24 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiPostgresql, SiTypescript, SiDjango } from "react-icons/si";
+import { FaGit, FaPython } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+} from "react-icons/fa";
+
+export const stackData = [
+  { title: "HTML", img: FaHtml5, },
+  { title: "CSS", img: FaCss3Alt, },
+  { title: "JS", img: FaJs, },
+  { title: "TypeScript", img: SiTypescript },
+  { title: "React", img: FaReact, },
+  { title: "Next.js", img: TbBrandNextjs },
+  { title: "NodeJS", img: FaNode, },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: SiDjango },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "Git", img: FaGit },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Felipe César",
+  githubUser: "fecef",
+  linkedinUser: "fecef",
+  whatsappNumber: "+5511945455167",
+  emailUser: "fecef.figueiredo@gmail.com.br",
+  instagramUser: "samuelleaoui",
+  facebookUser: "",
+};
